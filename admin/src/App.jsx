92f0a3e1b02effbe8d3d295ed25d6b1cc2b9bb61
ZahraQaisar <zqaisar427@gmail.com>
2026-02-11 18:40:07 +0500

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
@@ -13,7 +13,7 @@ export const currency = '$'
 
 const App = () => {
 
-  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'): '');
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
   useEffect(() => {
     localStorage.setItem('token', token)
@@ -23,7 +23,7 @@ const App = () => {
     <div className='bg-gray-50 min-h-screen'>
       <ToastContainer />
       {token === ""
-        ? <Login setToken={setToken}/>
+        ? <Login setToken={setToken} />
         : <>
           <Navbar setToken={setToken} />
           <hr />
@@ -31,6 +31,7 @@ const App = () => {
             <Sidebar />
             <div className='w-[70%] max-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
               <Routes>
+                <Route path='/' element={<Navigate to="/add" replace />} />
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
                 <Route path='/orders' element={<Orders token={token} />} />
